dc82e0a7ea0942da5a68b34cd05881301367eddd
Shashank-okali <shashankokali93@gmail.com>
2025-12-10 21:31:15 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,13 @@
+#Add Two Numbers
+# a=10
+# b=20
+# sum=a+b
+# print("sum=",sum)
+
+#Even odd 
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
