1ef84e9d6efd11b0b929ffbd80db323785219bed
Shashank-okali <shashankokali93@gmail.com>
2025-12-11 18:41:56 +0530

    practice

--- practice.py
+++ practice.py
@@ -30,3 +30,13 @@
 #     total += i
 
 # print("Sum =", total)
+
+# Factorial of a number
+
+num = int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, num+1):
+    fact *= i
+
+print("Factorial =", fact)
